c5b3194e5842d8321ccd7dccb619797beb112bbf
stetsonharding <stetson2001@msn.com>
2022-03-01 06:26:27 -0800

    added peek, empty, size, print, and clear methods.

--- stack.js
+++ stack.js
@@ -1,12 +1,10 @@
 //Building a stack data structure.
 
 class Stack {
-
     constructor(){
         this.items = [];
         this.count = 0;
     }
-
 
     //adds items to array, with the index of count.
     //increments count to allow more than one item to array.
@@ -28,21 +26,60 @@ class Stack {
        const newArr = this.items.slice(0, this.items.length - 1)
        this.items = newArr;
     
-       return deletedItem
+        console.log("Deleted item :" + deletedItem) 
+    }
+
+    //checks top element in the stack.
+    peek(){
+        if(this.count === 0) return undefined;
+        return this.items[this.count - 1]
+    }
+
+    //Check if stack is empty
+    emptyStack(){
+       return this.count === 0 ? 'Stack is empty' : `There are ${this.count} items on your stack`
     }
 
 
-  
+    //check stack size
+    size(){
+        console.log(`stack size: ${this.count}`)
+        return this.count;
+    }
 
+    //print elements in stack
+    print(){
+        let stack = "Items in your stack: "
+
+        for(let i = 0; i < this.count; i++){
+        stack += `${this.items[i]} `
+        }
+        console.log(stack)
+    }
+
+    //clears the stack
+    clear(){
+    this.items = []
+    this.count = 0;
+    return this.items;
+    }
 }
 
-const myStack = new Stack();
-myStack.push(100)
-myStack.push(200)
-myStack.push(300)
-myStack.pop();
-myStack.pop();
-myStack.pop();
-console.log(myStack.pop());
 
-console.log(myStack.items)
+
+//Testing methods.
+    const myStack = new Stack();
+    myStack.push(100)
+    myStack.push(200)
+    myStack.print();
+    myStack.push(300)
+    myStack.print();
+    myStack.pop();
+
+    console.log(myStack.peek())
+    console.log(myStack.emptyStack())
+
+
+    console.log(myStack.items)
+
+    console.log(myStack.clear())
